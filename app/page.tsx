348f73d414e6249099113055adbf22153f799dd8
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-03-23 16:39:56 +0100

    add name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full  font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
-      <div className="font-titleFont">amine iguider</div>
+      <div className="font-titleFont">amine iguider & Reda Ait Hammou</div>
     </main>
   );
 }
