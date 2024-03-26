48feb816a4b1cfd9b144ae4acf2312fb97ee3753
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-03-26 10:32:21 +0100

    add api

--- components/sections/Dishes.tsx
+++ components/sections/Dishes.tsx
@@ -1,13 +1,28 @@
-import { food_list } from "@/public/asstes";
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Dishe from "../elements/Dishe";
+import axios from "axios";
+import { typeDishe } from "@/types";
 
 const Dishes = () => {
+  const [data, setData] = useState<typeDishe[]>([]);
+  useEffect(() => {
+    axios
+      .get("/api/example-api")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <section className="space-y-6">
       <h1 className="font-titleFont font-bold text-4xl">Top Dishes near you</h1>
       <div className="grid grid-flow-row md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pb-4 ">
-        {food_list.slice(1, 10).map((item, index) => {
+        {data.slice(1, 10).map((item, index) => {
           return <Dishe key={index} item={item} />;
         })}
       </div>
